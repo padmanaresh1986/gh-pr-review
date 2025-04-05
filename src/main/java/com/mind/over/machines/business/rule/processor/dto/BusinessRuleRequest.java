67cf121c77c13b3114212dfd88d33aebacc11bcd
Padma Naresh <padmanaresh1986@gmail.com>
2025-04-05 21:32:12 +0530

    code fix

--- src/main/java/com/mind/over/machines/business/rule/processor/dto/BusinessRuleRequest.java
+++ src/main/java/com/mind/over/machines/business/rule/processor/dto/BusinessRuleRequest.java
@@ -3,12 +3,12 @@ package com.mind.over.machines.business.rule.processor.dto;
 import java.util.List;
 
 public class BusinessRuleRequest {
-    private String customerId;
-    private List<String> ruleNames;
+    public String customerId;
+    public List<String> ruleNames;
 
     public BusinessRuleRequest(String customerId, List<String> ruleNames) {
-        this.customerId = customerId;
-        this.ruleNames = ruleNames;
+        customerId = customerId;
+        ruleNames = ruleNames;
     }
 
     public String getCustomerId() {
